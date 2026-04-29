8d2a48093a274000884f3d66234c29a5251c6827
Andrew Tully <tully.werdnar@gmail.com>
2026-04-28 21:10:53 -0700

    update 13 of 04/28/2026

--- server.js
+++ server.js
@@ -927,8 +927,8 @@ app.get("/api/players", requireDatabase, async (req, res) => {
         p.discord_contact,
         p.created_at
       ORDER BY
-        adjusted_win_rate DESC,
         total_games DESC,
+        adjusted_win_rate DESC,
         p.name ASC
     `);
 
